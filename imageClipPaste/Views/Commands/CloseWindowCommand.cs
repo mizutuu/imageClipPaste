dde1bdf0cdbe02e944ea0d0b5350a2ef9a6e4cff
mizutuu <mizutuu@gmail.com>
2015-04-28 18:28:10 +0900

    Windowを閉じるコマンドを追加

---
+++ imageClipPaste/Views/Commands/CloseWindowCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+
+namespace imageClipPaste.Views.Commands
+{
+    /// <summary>
+    /// ウィンドウを閉じるコマンド
+    /// </summary>
+    public class CloseWindowCommand : CommandExtension
+    {
+        /// <summary>
+        /// コマンド実行時に呼び出されます。
+        /// </summary>
+        /// <param name="parameter">Windowオブジェクト</param>
+        public override void Execute(object parameter)
+        {
+            Window w = parameter as Window;
+            if (w == null)
+                throw new ArgumentNullException(
+                    "parameter",
+                    "XAMLのCommandParameterに、Windowインスタンスを指定して下さい。");
+
+            // コマンド発行元ウィンドウのクローズを実行します。
+            // window.Close() -> Closing -> Closedとイベントが流れていきます。
+            w.Close();
+        }
+    }
+}
---
+++ imageClipPaste/Views/Commands/CommandExtension.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace imageClipPaste.Views.Commands
+{
+    /// <summary>
+    /// XAMLから直接実行するコマンドの基盤クラスです。
+    /// </summary>
+    public abstract class CommandExtension : ICommand
+    {
+        /// <summary>
+        /// 現在の状態で、このコマンドが実行できるかを判定します。
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public virtual bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// RaiseCanExecuteChanged が呼び出されたときに生成されます。
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        /// <summary>
+        /// 現在のコマンドターゲットに対して、コマンドを実行します。
+        /// </summary>
+        /// <param name="parameter"></param>
+        public abstract void Execute(object parameter);
+    }
+}
