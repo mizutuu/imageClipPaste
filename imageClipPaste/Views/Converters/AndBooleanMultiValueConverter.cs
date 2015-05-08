876cf440990b7d312c6c4b9a9c3562e78af5f823
Hiroki Yoshida <mizutuu@gmail.com>
2015-05-08 21:30:03 +0900

    BooleanのANDをとるコンバータ追加

---
+++ imageClipPaste/Views/Converters/AndBooleanMultiValueConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace imageClipPaste.Views.Converters
+{
+    /// <summary>
+    /// 複数のBooleanのANDをとるコンバータ
+    /// </summary>
+    public class AndBooleanMultiValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool result = true;
+            foreach (object o in values)
+            {
+                bool? value = o as bool?;
+                if (!value.HasValue)
+                    throw new InvalidOperationException("bool型のみ許容されます。");
+
+                result &= value.Value;
+            }
+
+            return result;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
