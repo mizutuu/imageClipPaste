8dfaccdbec2f67ed8fc136810982af358931bcfa
Hiroki Yoshida <mizutuu@gmail.com>
2015-05-08 21:30:40 +0900

    App.config設定値を読み取るクラスを追加。

---
+++ imageClipPaste/AppSetting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.Configuration;
+
+namespace imageClipPaste
+{
+    /// <summary>
+    /// App.configファイルの、
+    /// <appSettings></appSettings>セクションに記載された設定値を取得します
+    /// </summary>
+    public static class AppSetting
+    {
+        public static bool IsEnableAnimation
+        {
+            get
+            {
+                var value = ConfigurationManager.AppSettings["IsEnableAnimation"];
+                if (String.IsNullOrWhiteSpace(value))
+                    return true;
+
+                return Convert.ToBoolean(value);
+            }
+        }
+    }
+}
