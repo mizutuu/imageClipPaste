6ca69255e48694f409b76ec692855e62a441516d
Hiroki Yoshida <mizutuu@gmail.com>
2015-05-09 13:08:58 +0900

    未使用プロパティの除去

--- imageClipPaste/Settings/PasteProcessInfo.cs
+++ imageClipPaste/Settings/PasteProcessInfo.cs
@@ -1,9 +1,5 @@
 ﻿using imageClipPaste.Enums;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace imageClipPaste.Settings
 {
@@ -17,13 +13,7 @@ namespace imageClipPaste.Settings
         /// Excelの場合はワークブック名
         /// PowerPointの場合は
         /// </summary>
-        public String Name { get; set; }
-
-        /// <summary>
-        /// Excelの場合はブックのパス、未保存の場合はnull
-        /// PowerPointの場合は
-        /// </summary>
-        public String Path { get; set; }
+        public string Name { get; set; }
 
         /// <summary>
         /// インスタンスハンドル
