d4e7127dac3f0c7f41899555231149eb5ca599e2
Hiroki Yoshida <mizutuu@gmail.com>
2015-05-09 13:10:19 +0900

    画像貼り付け追加

--- imageClipPaste/Models/Office/ExcelModel.cs
+++ imageClipPaste/Models/Office/ExcelModel.cs
@@ -1,9 +1,8 @@
 ﻿using imageClipPaste.Enums;
+using NLog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Excel = NetOffice.ExcelApi;
 
 namespace imageClipPaste.Models.Office
@@ -13,6 +12,9 @@ namespace imageClipPaste.Models.Office
     /// </summary>
     public class ExcelModel
     {
+        /// <summary>NLog</summary>
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
+
         /// <summary>
         /// 新しいExcelアプリケーションを取得します
         /// </summary>
@@ -20,7 +22,7 @@ namespace imageClipPaste.Models.Office
         /// Excelプロセスが取得できる場合は、取得したExcelアプリケーションを、
         /// 取得できない場合は、新しくExcelプロセスを立ち上げ、そのExcelアプリケーションを返却します
         /// </returns>
-        public static Excel.Application NewExcelApplication()
+        public static Excel.Application GetExcelApplication()
         {
             var app = Excel.Application.GetActiveInstance();
             if (app == null)
@@ -31,9 +33,6 @@ namespace imageClipPaste.Models.Office
                 {
                     Visible = true
                 };
-
-                // 新しいプロセスではワークブックが無いので追加します
-                app.Workbooks.Add();
             }
             return app;
         }
@@ -81,22 +80,20 @@ namespace imageClipPaste.Models.Office
                 return null;
 
             Excel.Workbook findBook = null;
-            using (var books = app.Workbooks)
-            {
-                books
-                    .ToList()
-                    .ForEach((book) =>
+
+            app.Workbooks
+                .ToList()
+                .ForEach((book) =>
+                {
+                    if (book.Name == name)
                     {
-                        if (book.Name == name)
-                        {
-                            findBook = book;
-                        }
-                        else
-                        {
-                            book.Dispose();
-                        }
-                    });
-            }
+                        findBook = book;
+                    }
+                    else
+                    {
+                        book.Dispose();
+                    }
+                });
 
             return findBook;
         }
@@ -108,26 +105,71 @@ namespace imageClipPaste.Models.Office
         public static List<Settings.PasteProcessInfo> GetPasteExcelProcessList()
         {
             List<Settings.PasteProcessInfo> result = new List<Settings.PasteProcessInfo>();
-            NetOffice.ExcelApi.Application[] applications = NetOffice.ExcelApi.Application.GetActiveInstances();
-            applications
-                .SelectMany(app => app.Workbooks)
-                .ToList()
-                .Where(book => !book.ReadOnly)
-                .ToList()
-                .ForEach(book =>
-                    result.Add(new Settings.PasteProcessInfo
-                    {
-                        Name = book.Name,
-                        Path = book.Path,
-                        HInstance = book.Application.Hinstance,
-                        HWnd = book.Application.Hwnd,
-                        PasteType = PasteType.Excel
-                    }));
+            Excel.Application[] applications = null;
+            try
+            {
+                applications = Excel.Application.GetActiveInstances();
+                applications
+                    .SelectMany(app => app.Workbooks)
+                    .ToList()
+                    .Where(book => !book.ReadOnly)
+                    .ToList()
+                    .ForEach(book =>
+                        result.Add(new Settings.PasteProcessInfo
+                        {
+                            Name = book.Name,
+                            HInstance = book.Application.Hinstance,
+                            HWnd = book.Application.Hwnd,
+                            PasteType = PasteType.Excel
+                        }));
 
-            foreach (var app in applications)
-                app.Dispose();
+                foreach (var app in applications)
+                    app.Dispose();
+
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("GetPasteExcelProcessList: ", ex);
+            }
 
             return result;
+        }
+
+        /// <summary>
+        /// 画像ファイルをワークシートに貼り付けます
+        /// </summary>
+        /// <param name="sheet">貼り付け先のワークシート</param>
+        /// <param name="path">貼り付ける画像ファイルパス</param>
+        public static void AddShapeFromImageFile(Excel.Worksheet sheet, string path)
+        {
+            AddShapeFromImageFile(sheet, path, 0, 0);
+        }
+
+        /// <summary>
+        /// 画像ファイルをワークシートの指定された位置に貼り付けます
+        /// </summary>
+        /// <param name="sheet">貼り付け先のワークシート</param>
+        /// <param name="path">貼り付ける画像ファイルパス</param>
+        /// <param name="top">Top座標</param>
+        /// <param name="left">Left座標</param>
+        public static void AddShapeFromImageFile(Excel.Worksheet sheet, string path, int top, int left)
+        {
+            // width, heightは、追加後にScaleを調整するので 0を指定します。
+            float width = 0, height = 0;
+
+            using (var shape = sheet.Shapes.AddPicture(
+                path,
+                NetOffice.OfficeApi.Enums.MsoTriState.msoFalse,
+                NetOffice.OfficeApi.Enums.MsoTriState.msoTrue,
+                left,
+                top,
+                width,
+                height))
+            {
+                // 貼り付けた画像の、拡大/縮小率を100%に設定します。
+                shape.ScaleHeight(1, NetOffice.OfficeApi.Enums.MsoTriState.msoTrue);
+                shape.ScaleWidth(1, NetOffice.OfficeApi.Enums.MsoTriState.msoTrue);
+            }
         }
     }
 }
