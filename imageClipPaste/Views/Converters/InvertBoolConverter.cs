55ba2c261462b3308941a933843e584cc3187414
Hiroki Yoshida <mizutuu@gmail.com>
2015-09-21 01:28:08 +0900

    booleanを反転するコンバータ追加

---
+++ imageClipPaste/Views/Converters/InvertBoolConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Windows.Data;
+
+namespace imageClipPaste.Views.Converters
+{
+    /// <summary>
+    /// Boolを反転するコンバータ
+    /// </summary>
+    public class InvertBoolConverter : IValueConverter
+    {
+        /// <summary>
+        /// 値を変換します。
+        /// </summary>
+        /// <param name="value">バインディング ソースによって生成された値。</param>
+        /// <param name="targetType">バインディング ターゲット プロパティの型。</param>
+        /// <param name="parameter">使用するコンバーター パラメーター。</param>
+        /// <param name="culture">コンバーターで使用するカルチャ。</param>
+        /// <returns>変換された値。</returns>
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value != null && value is bool)
+            {
+                return !((bool)value);
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// 値を変換します。
+        /// バインディング ターゲットの値をバインディング ソースに反映するときにこのメソッドを呼び出します。
+        /// </summary>
+        /// <param name="value">バインディング ターゲットによって生成される値。</param>
+        /// <param name="targetType">変換後の型。</param>
+        /// <param name="parameter">使用するコンバーター パラメーター。</param>
+        /// <param name="culture">コンバーターで使用するカルチャ。</param>
+        /// <returns>変換された値。</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return Convert(value, targetType, parameter, culture);
+        }
+    }
+}
